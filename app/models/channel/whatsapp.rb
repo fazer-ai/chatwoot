7a2570b6805d06f7cece0a516f268ee8e15d5660
CayoPOliveira <cayoproliveira@gmail.com>
2025-03-14 00:31:34 -0300

    feat: add 'baileys' as a provider option in Whatsapp channel model

--- app/models/channel/whatsapp.rb
+++ app/models/channel/whatsapp.rb
@@ -25,7 +25,7 @@ class Channel::Whatsapp < ApplicationRecord
   EDITABLE_ATTRS = [:phone_number, :provider, { provider_config: {} }].freeze
 
   # default at the moment is 360dialog lets change later.
-  PROVIDERS = %w[default whatsapp_cloud].freeze
+  PROVIDERS = %w[default whatsapp_cloud baileys].freeze
   before_validation :ensure_webhook_verify_token
 
   validates :provider, inclusion: { in: PROVIDERS }
