7cad98db20a8a45ba35479e0cef50f9db8a54a5b
CayoPOliveira <cayoproliveira@gmail.com>
2025-04-16 08:34:27 -0300

    chore: specs for stickers, audio and files

--- spec/services/whatsapp/incoming_message_baileys_service_spec.rb
+++ spec/services/whatsapp/incoming_message_baileys_service_spec.rb
@@ -321,6 +321,7 @@ describe Whatsapp::IncomingMessageBaileysService do
       end
 
       context 'when message type is image' do
+        let(:mimetype) { 'image/png' }
         let(:raw_message) do
           {
             key: { id: 'msg_123', remoteJid: '5511912345678@s.whatsapp.net', fromMe: false },
@@ -355,7 +356,6 @@ describe Whatsapp::IncomingMessageBaileysService do
 
         it 'attaches the media with expected behavior' do
           fixed_time = Time.zone.local(2023, 10, 10, 12, 34, 56)
-          time_str = fixed_time.strftime('%Y%m%d%H%M%S%s')
 
           allow(Time).to receive(:current).and_return(fixed_time)
 
@@ -365,16 +365,19 @@ describe Whatsapp::IncomingMessageBaileysService do
           message = conversation.messages.last
           attachment = message.attachments.last
 
-          expect(attachment).to be_present
-          expect(attachment.file_type).to eq('image')
-          expect(attachment.file).to be_present
+          filename = "image_#{message.id}#{fixed_time.strftime('%Y%m%d%H%M%S%s')}"
 
-          expect(attachment.file.filename.to_s).to eq("image_#{message.id}#{time_str}")
-          expect(attachment.file.content_type).to eq('image/png')
+          expect(attachment).to be_present
+          expect(attachment.file).to be_present
+          expect(attachment.file_type).to eq('image')
+
+          expect(attachment.file.filename.to_s).to eq(filename)
+          expect(attachment.file.content_type).to eq(mimetype)
         end
       end
 
       context 'when message type is video' do
+        let(:mimetype) { 'video/mp4' }
         let(:raw_message) do
           {
             key: { id: 'msg_123', remoteJid: '5511912345678@s.whatsapp.net', fromMe: false },
@@ -409,7 +412,6 @@ describe Whatsapp::IncomingMessageBaileysService do
 
         it 'attaches the media with expected behavior' do
           fixed_time = Time.zone.local(2023, 10, 10, 12, 34, 56)
-          time_str = fixed_time.strftime('%Y%m%d%H%M%S%s')
 
           allow(Time).to receive(:current).and_return(fixed_time)
 
@@ -419,12 +421,150 @@ describe Whatsapp::IncomingMessageBaileysService do
           message = conversation.messages.last
           attachment = message.attachments.last
 
+          filename = "video_#{message.id}#{fixed_time.strftime('%Y%m%d%H%M%S%s')}"
+
           expect(attachment).to be_present
+          expect(attachment.file).to be_present
           expect(attachment.file_type).to eq('video')
+
+          expect(attachment.file.filename.to_s).to eq(filename)
+          expect(attachment.file.content_type).to eq(mimetype)
+        end
+      end
+
+      context 'when message type is file' do
+        let(:filename) { 'file.pdf' }
+        let(:mimetype) { 'application/pdf' }
+        let(:raw_message) do
+          {
+            key: { id: 'msg_123', remoteJid: '5511912345678@s.whatsapp.net', fromMe: false },
+            message: { documentMessage: { fileName: filename } },
+            pushName: 'John Doe'
+          }
+        end
+        let(:params) do
+          {
+            webhookVerifyToken: webhook_verify_token,
+            event: 'messages.upsert',
+            data: {
+              type: 'notify',
+              messages: [raw_message]
+            },
+            extra: {
+              media: {
+                'msg_123' => 'JVBERi0xLjQKJVRlc3QgUERGCjEgMCBvYmoKPDwgL1R5cGUgL0NhdGFsb2cgL091dGxpbmVzIDIgMCBSIC9QYWdlcyAzIDAgUiA+PgplbmRvYmoKMiAwIG9iago8PCAvVHlwZSAvT3V0bGluZXMgL0NvdW50IDAgPj4KZW5kb2JqCjMgMCBvYmoKPDwgL1R5cGUgL1BhZ2VzIC9LaWRzIFs0IDAgUl0gL0NvdW50IDEgPj4KZW5kb2JqCjQgMCBvYmoKPDwgL1R5cGUgL1BhZ2UgCiAgIC9QYXJlbnQgMyAwIFIgCiAgIC9NZWRpYUJveCBbMCAwIDMwMCAyMDBdIAogICAvQ29udGVudHMgNSAwIFIgCiAgIC9SZXNvdXJjZXMgPDwgL0ZvbnQgPDwgL0YxIDw8IC9UeXBlIC9Gb250IC9TdWJ0eXBlIC9UeXBlMSAvQmFzZUZvbnQgL0hlbHZldGljYSA+PiA+PiA+PgplbmRvYmoKNSAwIG9iago8PCAvTGVuZ3RoIDUzID4+CnN0cmVhbQpCVAovRjEgMTIgVGYKNzIgMTUwIFRkCihIZWxsbyBQREYhKSBUagpFVAplbmRzdHJlYW0KZW5kb2JqCnhyZWYKMCA2CjAwMDAwMDAwMDAgNjU1MzUgZiAKMDAwMDAwMDAxNyAwMDAwMCBuIAowMDAwMDAwMDg0IDAwMDAwIG4gCjAwMDAwMDAxMzQgMDAwMDAgbiAKMDAwMDAwMDE5MyAwMDAwMCBuIAowMDAwMDAwMzkxIDAwMDAwIG4gCnRyYWlsZXIKPDwgL1NpemUgNiAvUm9vdCAxIDAgUiA+PgpzdGFydHhyZWYKNDk0CiUlRU9GCg==' # rubocop:disable Layout/LineLength
+              }
+            }
+          }
+        end
+
+        it 'attaches the media with expected behavior' do
+          described_class.new(inbox: inbox, params: params).perform
+
+          conversation = inbox.conversations.last
+          message = conversation.messages.last
+          attachment = message.attachments.last
+
+          expect(attachment).to be_present
+          expect(attachment.file_type).to eq('file')
           expect(attachment.file).to be_present
 
-          expect(attachment.file.filename.to_s).to eq("video_#{message.id}#{time_str}")
-          expect(attachment.file.content_type).to eq('video/mp4')
+          expect(attachment.file.filename.to_s).to eq(filename)
+          expect(attachment.file.content_type).to eq(mimetype)
+        end
+      end
+
+      context 'when message type is audio' do
+        let(:mimetype) { 'audio/opus' }
+        let(:raw_message) do
+          {
+            key: { id: 'msg_123', remoteJid: '5511912345678@s.whatsapp.net', fromMe: false },
+            message: { audioMessage: {} },
+            pushName: 'John Doe'
+          }
+        end
+        let(:params) do
+          {
+            webhookVerifyToken: webhook_verify_token,
+            event: 'messages.upsert',
+            data: {
+              type: 'notify',
+              messages: [raw_message]
+            },
+            extra: {
+              media: {
+                'msg_123' => 'T2dnUwACAAAAAAAAAAAAAAAAAAAAACqCBoIBE09wdXNIZWFkAQFoAIA+AAAAAABPZ2dTAAAAAAAAAAAAAAAAAAABAAAAjzLsvAEYT3B1c1RhZ3MIAAAAV2hhdHNBcHAAAAAAT2dnUwAAqOwAAAAAAAAAAAAAAgAAAJ5agxUOav8T7PLg/wTu9t3/DodLhgcIBwcaC+TBNuzFgAfJcifhROpQB8l5yMlXwAfJecjJUbCAAtRK//E6OBUjAL9sWX+ZId5v5ZPVuy3VNIvDjKNuehMtIhui23+8zF6nYoLkqYj4YaZXn4AigNd5FYRrpMiu/XTgerAwS4YuLSstKYrWH82ADKXlHmIp2IXekQmEI3IMggeTCD0zjPEsIjeqmkVfAP/lesBo7ZRZyGCLBaIvPCArr1eTRWfFCFYEeO+o+RQff9NFmkO6LC3YaJWaZBzcqpkiVSZF9uCK1eJTZkGsApQno7t/iJlJqd1hmGvZzGeg/G1nzB8ps1s9Cb34YpDVU9KAiuJYr1hUxmfxI4KFiX2UtYdxGWNxfGRuwTVTqHmIPie2mdzzibieN1+k/2p0iuOf3GMaWMWtIPc7iARhMUBobiunslmmvirb1LmEx6Y3HbqH2suQoMqK1dzcuF13w1aWz0u+oBY1+tTxacRxmjZG7F3XcO5KaLhjrsYUK8CUnXEe1Zf5gEuGJywoISSK1dzX4aeizrE8N98P1sp6HXFxZNvRhHfXyXbn8zATmP04ts2x34CKYFWmnvrO1ktfF2v9MAFnvuRVZsFSXR9Hk+aYisrdrzcR21sxONbuV+YtS4ph2d9VdNkVZysVa9vLS367WyDKLRFRpxJYpHmRa0EagqNkzyoH8YCBYpnXXDIoSnW++SQs+aRcDrN94k4h1BydvZWT6yHRZEQzCk6563SEL1aa+0XxjdWVM7Udxke5uGW38hTwN59v2YsjzGYzOcNqxLHAh8XEJTZ8Pmlpy+8syby3YVGYDViXZWlkk70CH6F8S4YjJiciKzMjXqKr6GIbeou0Z2zKCclT+8+O5A5ALPLMswUcqkJGyPSoif7hX1AOdSFD+LO+G67VA3stP4FyPxS/DJyhPVKTLxM/oL2BCHSKAGw5BpS4CTxuzlErqMvCx5mDnbPOQtUamiaMu+dEXFLkeL7SXUAy+TMfscZ0qO4p6NCYAKs4tltZPSA0LVmn+Dh/6jxOj4ZwiZWLM95xHaF8CODilYJ+fHMBfu6cQ+xgaw5wBeACJxrXhs97LFaJBrPcMIm2XzjUP0yjCkG5eR2BqOSczSe3XJYYPi07mjVreHddvl+1sJTviKMNahmb8oBLhisgIx8lifjnzQv9W7jolZGOf2l1ndLQ+M3B7RCSidTGpjvfyhtzjZ2V6myBp2X3gDMjLxurEFNSF9pVl2W3ooe08Tnbxq2bQQIjguzU9Q/AgUn5o2rDGKeGdCQSsmlvtHOAvAEvh7wxb2c10bc6GafiQDIE+0MP4cZvqz6hsFQoSRDxO7Rug+EXlQc58Plptx+JMTvG2AbTjVmIMEgYMo07jI5Ipp6wB4L0E3hWDuNir/Q2jDWJQzE6gTLV4xN1TQANxXiE7t3R0ReTRlXLN0iUvaMTn7/F1GUAZSodeEuGLy0pKieJMabgzX2fGflllvjvVX88N22aJYnLzaWHjGOQSVJh10ioHIIOtQG5fgnhxr26MIkoyMZKr/U1MqvWIwdUOmi+ZLGs8bgEMmeFoMUV0gLagpxgeWcX8o5VFajuLy9PtrhU29mheHxO7PHnIn1xsarpbsHLl7n36q3zVB1TFwPWHlDvcFB8LWkJ1JJptzK1O4NTc29JT7WaEGaBVnjX/6eUOddKPqZz/lmOQYD1T1kgL1e0+Zhp3fOmaWb+pSh+HuqPU2wkmgo7imUOQ0mj6wDMbFIeQObwL1HKAZJzCdeBsug573apU5qYuV/ZEqYGJ0hQ74HnQ6ZvwqjwU05LhiEnJiImL1FTafh3rc8Cy7o/1/fyfT503ZbIZ5u62VtXZ4YY6xZWL1cs2qHiYShorlN/L46omeX4MtXZzVW7pKCzQdBhCeIla+dDbUfBL1FPWDH1cDGKCYp+jho2Cl1bL7vlaPsUkGWZ6Hl+bo4fqG9dFNgvUVhK7goJwHOWkY9v9rKLGNlaZ90LoOflGShWQh5lmTmAiSpm7GJTFSCi2hFKwHjEsitafxXWgy5Cw4I/XbPK0qRhJMFlZ3CBEnqAp9a+8ecaIq4fxNaMQNtkQ3N2Ks0C+7NwWXuFKLLyR1PrDg8Koij1IrlngnjAS4YvKCUmJ4m2XO+ECBolT/hH0hBp9oXO+Lr8lxC00+X/WtVnmK1epbZTbxucayw2yIFy012giZJ1UCUkkOlt6j6VRuueVyyAtn2y+8lVoDn0uC4yXFr5D7bqqLZ5gDEhg/rhoxAc/iwGS7DXkmgCE4DCn4rZI+gOYeA4cqDyM3Ry0rAwpQbfmnN4rpT8chKcqdGSFaXArxYaVbiGE4xHzj1m+XohGDcswy6/+NUFh1V3ZNlNL9Z86Ro2Uymmr2II/V1r3jRW47zP9wpk0CiiCC1aitUOTkDxlo4/Tac7mIdUvffnW9PHNp1Nry+nHC4MenwcNBlAS4YlIiMhJi1nFjHrzqG7NcfjjR/pGQ2HYwnigLJnP/8P9H3bDY3XBiURNqAtaIomrMImJrOqzDDvFi7vN2t+kmPIjZUPsQpD9586WfXeLWiTq3EhFDlzQ3vlA9wvo98iUzWXYHbgUCsQLpnWKc5YO8AtaIoD4ebsRtvk2oXKaYjHlDKgBfT745WEq+W6DzJ4gbAtaQG0PqxSKfrdvBbpPPkYL8gV/fwQPR51jvwKWt72JlEms7KbgC1P3UVmRscoNd6wKJQqP0mdrYd6BaXv2Yq1uKW6LhjMXbip6gpLhigpJi0vLWfCbps6OrJt07ZXi+x+IfoTJTfmByn3TbOAjvImlHsFtYH6AON6iC20TbaYXrCU6mMbQ5931swaz0YPEao/mRwbKMjzivLz8iEUVPUOETfYiSrSWWJND57daMlpJoTRZ0/DyBLf/aFV6rph8o7AdeOZo+1lNcCBNMd2BGZ+HSuHZdzV6Tl/VteFnl9Rk5Yz9j9f+H87ODuzHFb2et/nlPjOZDCKYpy8X5WIm4WI/p0O98FNV6Dl6gTPjA1iFlvBRgdmvLatWrk9EokU74otEJAf04qGJ/SMBzLIkfizWs/IdMWnWWJfB4eJcIIBFetNFJsADa1eOurprIiS1UMzm/aa1JmyoEuDLC6LY53aB2ceiPT3EUnJCBYscjp1sKigjAvQRHGd0pLWcsf+Ktf3NKcS2CJnIIqy7J9E0MerTxxbnsqkg2w33fSv92Jepqtp3O+Xv77vv1aVBQReQgXh4DIxUcmJ/xjFW4ARWdxQ9sSyOwGNV0k7LLNflhOPxqJJ7cRUiEXHsKZhYr0rZA==' # rubocop:disable Layout/LineLength
+              }
+            }
+          }
+        end
+
+        it 'attaches the media with expected behavior' do
+          fixed_time = Time.zone.local(2023, 10, 10, 12, 34, 56)
+
+          allow(Time).to receive(:current).and_return(fixed_time)
+
+          described_class.new(inbox: inbox, params: params).perform
+
+          conversation = inbox.conversations.last
+          message = conversation.messages.last
+          attachment = message.attachments.last
+
+          filename = "audio_#{message.id}#{fixed_time.strftime('%Y%m%d%H%M%S%s')}"
+
+          expect(attachment).to be_present
+          expect(attachment.file_type).to eq('audio')
+          expect(attachment.file).to be_present
+
+          expect(attachment.file.filename.to_s).to eq(filename)
+          expect(attachment.file.content_type).to eq(mimetype)
+        end
+      end
+
+      context 'when message type is sticker' do
+        let(:mimetype) { 'image/png' }
+        let(:raw_message) do
+          {
+            key: { id: 'msg_123', remoteJid: '5511912345678@s.whatsapp.net', fromMe: false },
+            message: { stickerMessage: {} },
+            pushName: 'John Doe'
+          }
+        end
+        let(:params) do
+          {
+            webhookVerifyToken: webhook_verify_token,
+            event: 'messages.upsert',
+            data: {
+              type: 'notify',
+              messages: [raw_message]
+            },
+            extra: {
+              media: {
+                'msg_123' => 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII='
+              }
+            }
+          }
+        end
+
+        it 'attaches the media with expected behavior' do
+          fixed_time = Time.zone.local(2023, 10, 10, 12, 34, 56)
+
+          allow(Time).to receive(:current).and_return(fixed_time)
+
+          described_class.new(inbox: inbox, params: params).perform
+
+          conversation = inbox.conversations.last
+          message = conversation.messages.last
+          attachment = message.attachments.last
+
+          filename = "image_#{message.id}#{fixed_time.strftime('%Y%m%d%H%M%S%s')}"
+
+          expect(attachment).to be_present
+          expect(attachment.file_type).to eq('image')
+          expect(attachment.file).to be_present
+
+          expect(attachment.file.filename.to_s).to eq(filename)
+          expect(attachment.file.content_type).to eq(mimetype)
         end
       end
     end
