93d77e54ee8ffa4e768208afb9bbe547399bdd5f
CayoPOliveira <cayoproliveira@gmail.com>
2025-05-12 12:18:35 -0300

    fix: use last_seen_at message query conditions only if present

--- app/listeners/channel_listener.rb
+++ app/listeners/channel_listener.rb
@@ -29,8 +29,9 @@ class ChannelListener < BaseListener
     return unless channel.respond_to?(:send_read_messages)
 
     messages = conversation.messages.where(message_type: :incoming)
-                           .where('updated_at > ?', last_seen_at)
                            .where.not(status: :read)
+
+    messages = messages.where('updated_at > ?', last_seen_at) if last_seen_at.present?
 
     channel.send_read_messages(messages, conversation: conversation) if messages.any?
   end
