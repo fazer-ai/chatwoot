654c66f9cd9018b81c7f247d6652db6a6b136af1
Gabriel Jablonski <gabriel@fazer.ai>
2026-01-18 01:45:25 -0300

    ci: skip version check job execution (#29)

--- config/initializers/fazer_ai_sync_subscription.rb
+++ config/initializers/fazer_ai_sync_subscription.rb
@@ -4,6 +4,7 @@
 # the latest subscription features are available immediately after deploy.
 Rails.application.config.after_initialize do
   next unless Rails.env.production?
+  next if defined?(Rake) && Rake.application.top_level_tasks.any? { |task| task.start_with?('assets:') }
 
   Internal::CheckNewVersionsJob.perform_later(jitter_applied: true)
 end
