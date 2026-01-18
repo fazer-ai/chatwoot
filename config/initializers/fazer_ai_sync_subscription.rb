77cd6fd84abe5f98fb876ee404e6d82dd174945f
gabrieljablonski <contact@gabrieljablonski.com>
2026-01-18 01:57:35 -0300

    fix: correct Rake application check in fazer.ai sync initializer

--- config/initializers/fazer_ai_sync_subscription.rb
+++ config/initializers/fazer_ai_sync_subscription.rb
@@ -4,7 +4,7 @@
 # the latest subscription features are available immediately after deploy.
 Rails.application.config.after_initialize do
   next unless Rails.env.production?
-  next if defined?(Rake) && Rake.application.top_level_tasks.any? { |task| task.start_with?('assets:') }
+  next if defined?(Rake.application) && Rake.application.top_level_tasks.any? { |task| task.start_with?('assets:') }
 
   Internal::CheckNewVersionsJob.perform_later(jitter_applied: true)
 end
