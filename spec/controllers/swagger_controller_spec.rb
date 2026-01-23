cd9de8d2657394a397422d1fb8caf9eff7c67361
gabrieljablonski <contact@gabrieljablonski.com>
2026-01-23 11:32:34 -0300

    test: update swagger controller specs to correctly test redirection and response content

--- spec/controllers/swagger_controller_spec.rb
+++ spec/controllers/swagger_controller_spec.rb
@@ -2,11 +2,18 @@ require 'rails_helper'
 
 describe '/swagger', type: :request do
   describe 'GET /swagger' do
-    it 'renders swagger index.html' do
+    it 'redirects to /swagger/' do
       get '/swagger'
+      expect(response).to redirect_to('/swagger/')
+    end
+  end
+
+  describe 'GET /swagger/' do
+    it 'renders swagger index.html' do
+      get '/swagger/'
       expect(response).to have_http_status(:success)
-      expect(response.body).to include('redoc')
-      expect(response.body).to include('/swagger.json')
+      expect(response.body).to include('scalar')
+      expect(response.body).to include('swagger.json')
     end
   end
 end
